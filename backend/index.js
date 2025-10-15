4247efdaf8deb65fcb3047157d2c82a17109f104
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-15 21:46:18 +0700

    feat: add authmiddleware to the root of the folder

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import "reflect-metadata"
 import { connectDB } from "./server/config/db.js"
 import dotenv from "dotenv"
 import routerAuth from "./server/routes/AuthRoute.js"
+import routerTask from "./server/routes/TaskRoute.js"
+import authMiddleware from "./server/middlewares/authMiddleware.js"
 
 const app = express()
 const port = 3000
@@ -16,6 +18,7 @@ dotenv.config()
 
 app.get('/', (req, res) => { res.json("Hello Nexa!")});
 app.use('/api/v1/auth', routerAuth);
+app.use('/api/v1/task', authMiddleware, routerTask);
 
 app.listen(port, () => {
     console.log(`App listening to port ${port}`);
