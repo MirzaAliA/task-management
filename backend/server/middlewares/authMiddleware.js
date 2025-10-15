a89dd88a0e7086adffb5e3c1ba4dcc109d3fcf6f
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-15 19:01:09 +0700

    feat: add auth middleware
    add authmiddleware to check if user authorized or unauthorized

---
+++ backend/server/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+import { sendErrorResponse } from "../core/response";
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader?.split(" ")[1];
+
+    if(!token) return sendErrorResponse(res, { message: "No token provided!"}, 401);
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return sendErrorResponse(res, { message: "Invalid or expired token!"}, 403);
+        req.user = decoded;
+        next();
+    });
+}
+
+export default authMiddleware;
