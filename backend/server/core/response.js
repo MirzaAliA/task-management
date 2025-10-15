433cc2a6acf58142f29708dae121f1e62d08b8a3
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-15 17:58:20 +0700

    feat: add Success and Error Response

---
+++ backend/server/core/response.js
@@ -0,0 +1,14 @@
+export const sendOkResponse = (res, data = {}, message = "Success") => {
+    return res.status(200).json({
+        success: true,
+        message,
+        data
+    });
+};
+
+export const sendErrorResponse = (res, error, status = 500) => {
+    return res.status(status).json({
+        success: false,
+        message: error.message || "Something went wrong",
+    });
+};
