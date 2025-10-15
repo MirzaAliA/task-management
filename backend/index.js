72e5be320c716c47362c031bde7391f618d11f0c
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-15 16:05:16 +0700

    chore: initialize repository

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+const port = 3000
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => { res.json("Hello Nexa!")});
+
+app.listen(port, () => {
+    console.log(`App listening to port ${port}`);
+});
