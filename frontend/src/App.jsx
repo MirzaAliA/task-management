2a056c08a3e4cba365459b53c75904dea88db7d8
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-16 16:34:21 +0700

    fix: minor issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import LoginPage from '../components/LoginPage.jsx';
+import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './App.css'
-
+import RegisterPage from '../components/RegisterPage.jsx';
+import HomePage from '../components/HomePage.jsx';
+import TaskPage from '../components/TaskPage.jsx';
+import DetailTaskPage from '../components/DetailTaskPage.jsx';
 const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <LoginPage />
-    </QueryClientProvider>
-  )
-}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/tasks' element={<TaskPage />} />
+          <Route path='/tasks/:task_id' element={<DetailTaskPage />} />
 
-function LoginPage() {
-  return(
-    <>
-      <div>Hello World</div>
-    </>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
